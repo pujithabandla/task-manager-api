0244318eebc1acea0cddc7b069e0145b2b2ba91b
Pujitha Bandla <pujithabandla1253@gmail.com>
2026-04-18 00:16:00 -0400

    Added production config for deployment

--- app.py
+++ app.py
@@ -1,79 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from models.task import db
+from routes.task_routes import task_bp
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        "id": 1,
-        "title": "Learn Flask",
-        "description": "Build first backend API project",
-        "done": False
-    }
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+app.register_blueprint(task_bp)
 
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Task Manager API is running"
-    })
-
-
-@app.route("/tasks", methods=["GET"])
-def get_tasks():
-    return jsonify(tasks), 200
-
-
-@app.route("/tasks/<int:task_id>", methods=["GET"])
-def get_task(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            return jsonify(task), 200
-    return jsonify({"error": "Task not found"}), 404
-
-
-@app.route("/tasks", methods=["POST"])
-def create_task():
-    data = request.get_json()
-
-    if not data or "title" not in data or "description" not in data:
-        return jsonify({"error": "Title and description are required"}), 400
-
-    new_task = {
-        "id": len(tasks) + 1,
-        "title": data["title"],
-        "description": data["description"],
-        "done": False
-    }
-
-    tasks.append(new_task)
-    return jsonify(new_task), 201
-
-
-@app.route("/tasks/<int:task_id>", methods=["PUT"])
-def update_task(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            data = request.get_json()
-
-            task["title"] = data.get("title", task["title"])
-            task["description"] = data.get("description", task["description"])
-            task["done"] = data.get("done", task["done"])
-
-            return jsonify(task), 200
-
-    return jsonify({"error": "Task not found"}), 404
-
-
-@app.route("/tasks/<int:task_id>", methods=["DELETE"])
-def delete_task(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            tasks.remove(task)
-            return jsonify({"message": "Task deleted successfully"}), 200
-
-    return jsonify({"error": "Task not found"}), 404
+    return {"message": "Task Manager API (structured) running"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
